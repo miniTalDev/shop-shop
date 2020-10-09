e71b1ccd0d707f60b88e860b9c931355d423c3be
nasir522004 <nasir522004@gmail.com>
2020-10-08 19:03:32 -0700

    Implement Global State

--- client/src/App.js
+++ client/src/App.js
@@ -2,46 +2,51 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-
+import { Provider } from 'react-redux';
+import store from './utils/store';
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Nav from "./components/Nav/index";
+import Nav from "./components/Nav";
+import Success from "./pages/Success";
+// import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from "./pages/OrderHistory";
 
 const client = new ApolloClient({
-  request: (operation) => {
-    const token = localStorage.getItem('id_token')
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    })
-  },
-  uri: '/graphql',
+    request: (operation) => {
+        const token = localStorage.getItem('id_token')
+        operation.setContext({
+            headers: {
+                authorization: token ? `Bearer ${token}` : ''
+            }
+        })
+    },
+    uri: '/graphql',
 })
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/orderHistory" component={OrderHistory} />
-            <Route exact path="/products/:id" component={Detail} />
-            <Route component={NoMatch} />
-          </Switch>
-        </div>
-      </Router>
-    </ApolloProvider>
-
-  );
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <div>
+                    <Provider store={store}>
+                        <Nav />
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/login" component={Login} />
+                            <Route exact path="/signup" component={Signup} />
+                            <Route exact path="/orderHistory" component={OrderHistory} />
+                            <Route exact path="/products/:id" component={Detail} />
+                            <Route exact path="/success" component={Success} />
+                            <Route component={NoMatch} />
+                        </Switch>
+                    </Provider>
+                </div>
+            </Router>
+        </ApolloProvider>
+    );
 }
 
 export default App;
---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,24 @@
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers';
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        cart: [],
+        cartOpen: false,
+        categories: [],
+        currentCategory: ''
+    });
+    // use this to confirm it works!
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
